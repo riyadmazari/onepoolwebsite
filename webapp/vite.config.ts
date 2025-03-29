848a410c15dbc7930c13ff8449cd91274194a6f6
riyadmazari <rmazari.ieu2021@student.ie.edu>
2025-03-29 16:16:48 +0100

    Clean push without unnecessary files

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,33 +1,24 @@
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// @ts-ignore
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  // Base path for GitHub Pages or custom hosting
-  base: '/app/',  
-  build: {
-    sourcemap: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    fs: {
-      strict: true,
-    },
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    sourcemap: true,
   },
 });
